bc452c3f4953e53f591f6d79a56bf303335b2f75
xyz <mugen.xyz@gmail.com>
2025-06-13 06:26:02 +0900

    feat: add GitHub Actions workflows for CI, build, and deployment, update package.json with package manager, and configure dependabot

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/fake-sdgs-generator/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
